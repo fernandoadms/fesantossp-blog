945056eaee7fad77facfe7330ca62cf14b56edc6
Fernando Alves <fernando.alves@jussi.com.br>
2020-12-13 00:14:14 -0300

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>404 Page</title>
+      <h1>404 Page</h1>
+    </main>
+  )
+}
+
+export default IndexPage
