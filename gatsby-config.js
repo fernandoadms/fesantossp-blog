2e64d7bf7bcda7fbb224e6498bed20e10a90ebb1
Fernando Alves <fernando.alves@jussi.com.br>
2020-12-18 01:53:00 -0300

    Add Markdown Remark plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,26 @@ module.exports = {
     url: 'https://github.com/fesantossp/fesantossp',
     image: 'https://avatars3.githubusercontent.com/u/6516578?s=460&u=0a6ea4b4449800984937bf48e9714e73d35b0a7f&v=4',
     twitterUsername: 'https://twitter.com/fesantossp'
-  }, 
+  },
   plugins: [
-     {
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: path.join(__dirname, `src`, `images`),
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/posts`,
       },
     },
     'gatsby-plugin-netlify-cms',
