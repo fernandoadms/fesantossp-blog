5d4182f02b45fe118cd23555590de1961b4319ef
Fernando Alves <fernando.alves@jussi.com.br>
2020-12-18 02:09:53 -0300

    Get Data from GraphQL

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,56 @@
 import * as React from 'react'
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
 import PostItem from '../components/PostItem'
 
 const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+    query PostList {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              background
+              category
+              description
+              title
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+            }
+            timeToRead
+          }
+        }
+      }
+    }
+  `)
+
+  const postList = allMarkdownRemark.edges
+  
   return (
     <Layout>
       <SEO title='Home' />
-      
-      <PostItem
-        slug="/about/"
-        category="Misc"
-        date="30 de Julho de 2019"
-        timeToRead="5"
-        title="Diga não ao Medium: tenha sua própria plataforma"
-        description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
-      />
+      {postList.map(({
+        node: {
+          frontmatter: {
+            background,
+            category,
+            date,
+            description,
+            title },
+          timeToRead },
+      }) => (
+        <PostItem
+          background={background}
+          slug={"/about/"}
+          category={category}
+          date={date}
+          timeToRead={timeToRead}
+          title={title}
+          description={description}
+        />
+      ))}
+
     </Layout>
   )
 }
