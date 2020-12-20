4db358e58a2e2d48b632542d1781808f10cafd0e
Fernando Alves <fernando.alves@jussi.com.br>
2020-12-20 17:21:38 -0300

    Create Search page

---
+++ src/pages/search.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Layout from '../components/Layout'
+import Search from '../components/Search'
+
+const SearchPage = () => {
+  return (
+    <Layout>
+      <Search />
+    </Layout>
+  )
+}
+
+export default SearchPage
